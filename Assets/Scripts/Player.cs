50736baceb4f0e44b371606d4ce0880b66025be8
JokingJester <81639209+JokingJester@users.noreply.github.com>
2021-10-12 12:25:39 -0500

    Flip Sprite
    When the left arrow key is pressed, the sprite will face left. When the right arrow key is pressed, the sprite will face right.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,11 +10,13 @@ public class Player : MonoBehaviour
 
     private PlayerAnimation _playerAnim;
     private Rigidbody2D _rb;
+    private SpriteRenderer _renderer;
 
     void Start()
     {
         _playerAnim = GetComponent<PlayerAnimation>();
         _rb = GetComponent<Rigidbody2D>();
+        _renderer = GetComponentInChildren<SpriteRenderer>();
     }
 
     void Update()
@@ -25,12 +27,21 @@ public class Player : MonoBehaviour
     private void Movement()
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
-        _playerAnim.Move(horizontalInput);
         _rb.velocity = new Vector2(horizontalInput * _speed, _rb.velocity.y);
         if (Input.GetKey(KeyCode.Space) && IsGrounded() == true)
         {
             _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
         }
+        _playerAnim.Move(horizontalInput);
+        FlipSprite(horizontalInput);
+    }
+
+    private void FlipSprite(float input)
+    {
+        if (input > 0)
+            _renderer.flipX = false;
+        else if (input < 0)
+            _renderer.flipX = true;
     }
 
     bool IsGrounded()
