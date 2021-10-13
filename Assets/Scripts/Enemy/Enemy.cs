4526b99068ef6a96b59d4378599b085d099458e7
JokingJester <81639209+JokingJester@users.noreply.github.com>
2021-10-13 11:46:26 -0500

    Created Enemy, Moss Giant, And Spider Classes

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] protected int health;
+    [SerializeField] protected float speed;
+    [SerializeField] protected int gems;
+    [SerializeField] protected Transform pointA, pointB;
+
+    protected virtual void Attack()
+    {
+
+    }
+}
