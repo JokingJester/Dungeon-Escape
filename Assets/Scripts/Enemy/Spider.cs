4780d2063a87fc44b2abcd0190635f21da4e4553
JokingJester <81639209+JokingJester@users.noreply.github.com>
2021-10-13 20:32:09 -0500

    Programmed The Spider AI
    I programmed it a different way but currently in this commit it acts the same as the Moss Giant AI.

--- Assets/Scripts/Enemy/Spider.cs
+++ Assets/Scripts/Enemy/Spider.cs
@@ -4,8 +4,45 @@ using UnityEngine;
 
 public class Spider : Enemy
 {
+    private Animator _anim;
+    private bool _canFlip;
+    private SpriteRenderer _renderer;
+    private Transform _targetPos;
+
+    private void Start()
+    {
+        _renderer = GetComponentInChildren<SpriteRenderer>();
+        _anim = GetComponentInChildren<Animator>();
+        _targetPos = pointA;
+    }
     public override void Update()
     {
-        
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Idle") == false)
+        {
+            if (_canFlip == true)
+            {
+                _canFlip = false;
+                _renderer.flipX = !_renderer.flipX;
+            }
+
+            transform.position = Vector2.MoveTowards(transform.position, _targetPos.position, speed * Time.deltaTime);
+            float distance = Vector2.Distance(transform.position, _targetPos.position);
+
+            if (distance < 0.1f)
+            {
+                if (_targetPos.position == pointA.position)
+                    _targetPos = pointB;
+                else
+                    _targetPos = pointA;
+
+                _anim.SetTrigger("Idle");
+                _canFlip = true;
+            }
+        }
     }
 }
