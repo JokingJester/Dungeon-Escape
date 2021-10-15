f7f7fed8a3aef58219458282a99b33e63bc81815
JokingJester <81639209+JokingJester@users.noreply.github.com>
2021-10-14 19:45:41 -0500

    Player Hitbox Can Detect Attacked Enemies

---
+++ Assets/Scripts/Player/Attack.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+    }
+}
