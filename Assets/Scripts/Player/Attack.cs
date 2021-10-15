6a890795260ed5fe5a95fd7fee2ae0ce235ca284
JokingJester <81639209+JokingJester@users.noreply.github.com>
2021-10-15 14:28:35 -0500

    Sword Hitbox Can't Damage The Enemy Multiple Times During Animation

--- Assets/Scripts/Player/Attack.cs
+++ Assets/Scripts/Player/Attack.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
+    private bool _canDamage = true;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         IDamageable hit = collision.GetComponent<IDamageable>();
-        if(hit != null)
+        if (hit != null && _canDamage == true)
         {
+            StartCoroutine(DamageCooldown());
             hit.Damage();
         }
+    }
+
+
+    private IEnumerator DamageCooldown()
+    {
+        _canDamage = false;
+        yield return new WaitForSeconds(0.5f);
+        _canDamage = true;
     }
 }
